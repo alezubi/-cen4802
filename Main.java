c7bdd7582899a711ede7c488e1f9b1d7f9265ae3
alezubi <alejandro.zubillaga@hotmail.com>
2025-01-07 13:09:55 -0500

    Added Javadoc comments to the fibonacci method. Related to issue #1

--- Main.java
+++ Main.java
@@ -1,14 +1,19 @@
 public class Main {
 
-
-
     /**
      * Calculates the nth term in the Fibonacci sequence.
      *
-     * @param n the term of the Fibonacci sequence to calculate
+     * This method uses a recursive approach to compute the Fibonacci number
+     * at the specified position in the sequence.
+     *
+     * @param n the term of the Fibonacci sequence to calculate (must be non-negative)
      * @return the nth term in the Fibonacci sequence
+     * @throws IllegalArgumentException if n is negative
      */
     public static int fibonacci(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Input must be a non-negative integer.");
+        }
         if (n <= 1) {
             return n;
         }
